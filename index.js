3e2082c15977514d203fc3d54bd82af46e9b4c7a
danielkordel <daniel.kordel@gmail.com>
2022-01-08 14:12:25 +0100

    port changed to environmental variable

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const fetch = require('node-fetch');
 require('dotenv').config(); // adding environmental variables usage
 
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Starting server at ${port}`));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
